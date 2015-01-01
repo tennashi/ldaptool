d1050388cb416c7ba54e91dd7c19eb63daa3b38b
tennashi <yuya.gt@gmail.com>
2015-01-02 00:24:58 +0900

    fixed

--- modloglevel2.c
+++ modloglevel2.c
@@ -2,101 +2,12 @@
 #include <ldap.h>
 #include <math.h>
 
-int checkArgs(int length, char* argv)
-{
-  int i;
-  float n;
-  for (i = 1; i < length; i++) {
-    n = atof(argv[i]);
-    if (n > 32768) {
-      return 1;
-    }
-    if (n == -1) {
-      break;
-    }
-    while (1) {
-      if (n == 1) {
-        break;
-      }
-      n = n / 2;
-      if (n < 1) {
-        return 1;
-      }
-    }
-  }
-  return 0;
-}
-
-int sumArgs(int length, char* argv) {
-  int sum = 0;
-  int i;
-  int n;
-  for (i = 1; i < length; i++) {
-    n = atoi(argv[i]);
-    sum = sum + n;
-  }
-}
-
-LDAP *ldapConnection()
-{
-  LDAP *ld;
-  ld = ldap_init("localhost", LDAP_PORT);
-  if (ld == NULL) {
-    return ld;
-  }
-
-  char *url = "ldapi:///";
-  ldap_set_option(ld, LDAP_OPT_URI, url);
-
-  int version = LDAP_VERSION3;
-  ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
-
-  return ld;
-}
-
-int ldapBind(LDAP *ld)
-{
-  int status;
-  status = ldap_simple_bind_s(ld, "cn=config", "secret");
-  if (status != LDAP_SUCCESS) {
-    return 1;
-  }
-  return 0;
-}
-
-int ldapModLoglevel(LDAP *ld, char level)
-{
-  LDAPMod mod;
-  LDAPMod *mods[2];
-  char *vals[2];
-
-  /* 更新データ */
-  vals[0] = level;
-  vals[1] = NULL;
-  mod.mod_op = LDAP_MOD_REPLACE;
-  mod.mod_type = "olcLogLevel";
-  mod.mod_values = vals;
-  mods[0] = &mod;
-  mods[1] = NULL;
-
-  int status;
-  status = ldap_modify_ext_s(ld, "cn=config", mods, NULL, NULL);
-  if (status != LDAP_SUCCESS) {
-    return 1;
-  }
-  return 0;
-}
-
-int ldapUnbind(LDAP *ld)
-{
-  int status;
-  status = ldap_unbind_ext_s(ld, NULL, NULL);
-  if (status != LDAP_SUCCESS) {
-    return 1;
-  }
-  return 0;
-}
-
+int checkArgs(int, char*);
+int sumArgs(int, char*);
+LDAP *ldapConnection(void);
+int ldapBind(LDAP *ld);
+int ldapModLoglevel(LDAP *ld, char*);
+int ldapUnbind(LDAP *ld);
 
 int main(int argc, char* argv[])
 {
@@ -145,6 +56,102 @@ int main(int argc, char* argv[])
   }
   return 0;
 }
+
+int checkArgs(int length, char* argv)
+{
+  int i;
+  float n;
+  for (i = 1; i < length; i++) {
+    n = atof(argv[i]);
+    if (n > 32768) {
+      return 1;
+    }
+    if (n == -1) {
+      break;
+    }
+    while (1) {
+      if (n == 1) {
+        break;
+      }
+      n = n / 2;
+      if (n < 1) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+int sumArgs(int length, char* argv) {
+  int sum = 0;
+  int i;
+  int n;
+  for (i = 1; i < length; i++) {
+    n = atoi(argv[i]);
+    sum = sum + n;
+  }
+}
+
+LDAP *ldapConnection(void)
+{
+  LDAP *ld;
+  ld = ldap_init("localhost", LDAP_PORT);
+  if (ld == NULL) {
+    return ld;
+  }
+
+  char *url = "ldapi:///";
+  ldap_set_option(ld, LDAP_OPT_URI, url);
+
+  int version = LDAP_VERSION3;
+  ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
+
+  return ld;
+}
+
+int ldapBind(LDAP *ld)
+{
+  int status;
+  status = ldap_simple_bind_s(ld, "cn=config", "secret");
+  if (status != LDAP_SUCCESS) {
+    return 1;
+  }
+  return 0;
+}
+
+int ldapModLoglevel(LDAP *ld, char *level)
+{
+  LDAPMod mod;
+  LDAPMod *mods[2];
+  char *vals[2];
+
+  /* 更新データ */
+  vals[0] = level;
+  vals[1] = NULL;
+  mod.mod_op = LDAP_MOD_REPLACE;
+  mod.mod_type = "olcLogLevel";
+  mod.mod_values = vals;
+  mods[0] = &mod;
+  mods[1] = NULL;
+
+  int status;
+  status = ldap_modify_ext_s(ld, "cn=config", mods, NULL, NULL);
+  if (status != LDAP_SUCCESS) {
+    return 1;
+  }
+  return 0;
+}
+
+int ldapUnbind(LDAP *ld)
+{
+  int status;
+  status = ldap_unbind_ext_s(ld, NULL, NULL);
+  if (status != LDAP_SUCCESS) {
+    return 1;
+  }
+  return 0;
+}
+
 
 
 
